33c0488843447c7296c19549f8cead841aa7fad0
Thev2Andy <ciprian.andrei002@gmail.com>
2021-08-26 12:38:18 +0300

    Added a BG Controller.

---
+++ Assets/Scripts/Background Controller/BackgroundController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundController : MonoBehaviour
+{
+    public Image BGImage;
+    public static BackgroundController Instance;
+
+    private void Awake()
+    {
+        if (Instance != this) Destroy(Instance);
+        Instance = this;
+    }
+
+    public void ChangeBackground(Sprite image, Color color)
+    {
+        BGImage.sprite = image;
+        BGImage.color = color;
+    }
+}
---
+++ Assets/Scripts/Triggers/BackgroundTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTrigger : MonoBehaviour
+{
+    public Sprite BGImage;
+    public Color BGColor;
+
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.CompareTag("Player"))
+        {
+            BackgroundController.Instance.ChangeBackground(BGImage, BGColor);
+        }
+    }
+}
